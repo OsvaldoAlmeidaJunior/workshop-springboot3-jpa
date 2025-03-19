5e04236de54fe2cdfcc57cb5d00daec4eb03acd1
Osvaldo <osvaldo.junior13@gmail.com>
2025-03-18 23:18:25 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/projetoosvaldo1/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projetoosvaldo1.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoosvaldo1.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>  {
+
+}

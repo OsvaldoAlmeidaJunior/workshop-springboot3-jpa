b9e851fd6834701b098ff36a327854c804bfeb8e
Osvaldo <osvaldo.junior13@gmail.com>
2025-03-20 00:34:42 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/projetoosvaldo1/course/entities/User.java
+++ src/main/java/com/projetoosvaldo1/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.projetoosvaldo1.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,6 +17,7 @@ import jakarta.persistence.Table;
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy  = GenerationType.IDENTITY)
 	private Long id;
@@ -21,6 +25,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 
 	public User() {
 	}
@@ -32,6 +40,8 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+		
+		
 	}
 
 	public Long getId() {
@@ -73,6 +83,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,5 +104,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 }
